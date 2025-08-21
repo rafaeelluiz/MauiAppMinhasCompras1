a789ea05b4ab2b8cb6a094c071bc3ba6235fb5ff
rafael <rafanasciluiz@outlook.com>
2025-08-21 16:24:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras1/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiAppMinhasCompras1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
